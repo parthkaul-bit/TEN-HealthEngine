84c687017306f30742a806f8049637324ced3f27
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-12-18 15:59:08 +0530

    removed boilerplate code

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Welcome to Health Engine API!");
 });
 
 const PORT = 5000;
